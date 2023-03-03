0a53b3f8226623210ceee799c83c44b5ee883436
rizk1 <srizki4455@gmail.com>
2023-03-03 23:15:08 +0700

    add db seeder for product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::insert([
+            ['product_code' => 'SKP12', 'product_name' => 'So Klin Pewangi', 'price' => 15000, 'currency' => 'IDR', 'discount' => 10, 'dimension' => '13 cm x 10 cm', 'unit' => 'PCS'],
+            ['product_code' => 'SKP13', 'product_name' => 'Daia Pemutih', 'price' => 15000, 'currency' => 'IDR', 'discount' => 25, 'dimension' => '13 cm x 10 cm', 'unit' => 'PCS'],
+            ['product_code' => 'SKP14', 'product_name' => 'Ale - Ale', 'price' => 35000, 'currency' => 'IDR',  'discount' => null, 'dimension' => '13 cm x 10 cm', 'unit' => 'BOX'],
+            ['product_code' => 'SKP15', 'product_name' => 'Mie Sedap Goreng', 'price' => 43000, 'currency' => 'IDR', 'discount' => null, 'dimension' => '13 cm x 10 cm', 'unit' => 'BOX'],
+        ]);
     }
 }
